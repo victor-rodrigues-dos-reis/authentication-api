da09ecdf1f8fd10f1fd59758b9f69ad3e2548900
Victor Rodrigues dos Reis <victor_rodrigues_dos_reis@hotmail.com>
2021-03-13 19:54:03 -0300

    Criado a model da tabela de black list dos tokens
    Quando o usuário da logout no sistema o token informado entra nessa tabela, assim quando for verificado o token ele checa se o token informado não está nessa tabela.
    Desse modo impossibilita de utilizarem um token que já foi "descartado".

---
+++ src/models/TokenBlackList.js
@@ -0,0 +1,29 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('../database');
+const crypto = require('crypto');
+
+// CRIA A ESTRUTURA DA TABELA "token_black_list"
+const TokenBlackListSchema = sequelize.define('token_black_list', {
+    id:{ 
+        type:Sequelize.INTEGER, 
+        autoIncrement:true, 
+        allowNull:false, 
+        primaryKey:true
+    }, 
+    token: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    createdAt: Sequelize.DATE,
+},{
+    updatedAt: false,
+});
+
+// Cria o hash do token antes de adicioná-lo a lista negra
+TokenBlackListSchema.beforeCreate(async (tokenBlackList, options) => {
+    const hashed = crypto.createHash('md5').update(tokenBlackList.token).digest('hex');;
+    tokenBlackList.token = hashed;
+});
+
+module.exports = TokenBlackListSchema;
