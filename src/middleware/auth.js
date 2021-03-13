08f72f1c71b6ead256290063f6188662ff1e092b
Victor Rodrigues dos Reis <victor_rodrigues_dos_reis@hotmail.com>
2021-03-13 20:10:56 -0300

    Criado middleware de validação do token
    O arquivo verifica se o token informado é válido antes de entrar na rota

---
+++ src/middleware/auth.js
@@ -0,0 +1,63 @@
+const jwt = require('jsonwebtoken');
+const authConfig = require('../config/auth');
+const TokenBlackList = require('../models/TokenBlackList');
+const crypto = require('crypto');
+
+module.exports = async (request, response, next) => {
+    const authHeader = request.headers.authorization;
+    let decodedToken;
+
+
+
+    
+    // Verifica se foi enviado um token na requisição
+    if (!authHeader)
+        return response.status(400).json({'error': 'No tokens provided'});
+    
+
+
+
+    const parts = authHeader.split(' ');
+
+    // Verifica se o token NÃO contém 2 partes
+    // Normalmente um token tem uma extrutura semelhante a isso "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IlZpY3RvclJlaXMiLCJpYXQiOjE2MTU2NjA3OTQsImV4cCI6MTYxNTc0NzE5NH0.makGJQSy-EG66Y4dibh4ugNFy-kaC8r4ziDj7RWNBNQ"
+    if (parts.length !== 2)
+        return response.status(400).json({'error': 'Badly formatted token'});
+
+
+
+
+    const [scheme, token] = parts;
+
+    // Verifica se a primeira parte do token contém a palavra "Bearer"
+    if (!scheme.includes('Bearer'))
+        return response.status(400).json({'error': 'Badly formatted token'});
+
+
+
+
+    const tokenHashed = crypto.createHash('md5').update(authHeader).digest('hex');
+
+    // Pesquisa pelo token na blacklist
+    const tokenExists = await TokenBlackList.findOne({where: {token: tokenHashed}});
+
+    // Verifica se o token ja foi desativado
+    if (tokenExists)
+        return response.status(401).json({'error': 'Token disabled'});
+
+
+
+
+    // Verifica se o token informado é válido
+    try {
+        decodedToken = jwt.verify(token, authConfig.secret);
+
+    } catch (error) {
+        return response.status(401).json({'error': 'Invalid token'});
+    }
+    
+    // Adiciona o conteúdo do token ao parâmetro request
+    request.token = decodedToken;
+
+    return next();
+}
